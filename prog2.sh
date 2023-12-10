7d2e10859f1759ed039f6877ae57eb4e8c3b9ef6
lenovo <udad16cs@gmail.com>
2023-12-10 19:32:00 +0530

    num

--- prog2.sh
+++ prog2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "you have entered $# number of arguments"
 echo "the arguments are $*"
-echo "the first argument is : $1"
+echo "the first argument is : $3"
 echo "the last argument is : $x"
 echo "shifting by one"
-shift 1
+shift 3
 echo "no the positional parameters after shift: ${*}"
 echo "the program name is: $0"
 #end
